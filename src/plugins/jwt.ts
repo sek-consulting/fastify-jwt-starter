5108bd76f087a6ff5225891c903eaf16e425aa93
e-k-agentur <stefan.eideloth@gmail.com>
2023-03-06 13:57:05 +0100

    fixed previous merge conflict

--- src/plugins/jwt.ts
+++ src/plugins/jwt.ts
@@ -1,30 +1,30 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-import fp from "fastify-plugin";
+import { FastifyReply, FastifyRequest } from "fastify"
+import fp from "fastify-plugin"
 
-import { AccessTokenPayload } from "../entities/jwt-payloads";
-import { verifyToken } from "../services/auth";
+import { AccessTokenPayload } from "../entities/jwt-payloads"
+import { verifyToken } from "../services/auth"
 
 /**
  * This plugins handles authentication
  */
 export default fp(async (server) => {
   server.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
-    const authHeader = request.headers.authorization;
+    const authHeader = request.headers.authorization
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return reply.unauthorized();
+      return reply.unauthorized()
     }
     try {
-      const token = authHeader.substring(7);
-      server.jwtPayload = verifyToken(token) as AccessTokenPayload;
+      const token = authHeader.substring(7)
+      server.jwtPayload = verifyToken(token) as AccessTokenPayload
     } catch (err) {
-      return reply.status(401).send(err); // unauthorized
+      return reply.status(401).send(err) // unauthorized
     }
-  });
-});
+  })
+})
 
 declare module "fastify" {
   export interface FastifyInstance {
-    authenticate(): void;
-    jwtPayload: AccessTokenPayload;
+    authenticate(): void
+    jwtPayload: AccessTokenPayload
   }
 }
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,31 +1,31 @@
-import { FastifyPluginAsync, FastifyRequest } from "fastify";
+import { FastifyPluginAsync, FastifyRequest } from "fastify"
 
-import { createTokens, login, refreshTokens } from "../../services/auth";
+import { createTokens, login, refreshTokens } from "../../services/auth"
 
 const auth: FastifyPluginAsync = async (server) => {
   server.post(
     "/login",
     async (request: FastifyRequest<{ Body: { email: string; password: string } }>, reply) => {
-      const user = await login(request.body.email, request.body.password);
+      const user = await login(request.body.email, request.body.password)
       if (user) {
-        const tokens = await createTokens(user.id);
-        return reply.status(200).send(tokens);
+        const tokens = await createTokens(user.id)
+        return reply.status(200).send(tokens)
       }
-      return reply.unauthorized();
+      return reply.unauthorized()
     }
-  );
+  )
 
   server.post(
     "/refresh",
     async (request: FastifyRequest<{ Body: { refreshToken: string } }>, reply) => {
       try {
-        const tokens = await refreshTokens(request.body.refreshToken);
-        return reply.status(200).send(tokens);
+        const tokens = await refreshTokens(request.body.refreshToken)
+        return reply.status(200).send(tokens)
       } catch (err) {
-        return reply.status(401).send(err);
+        return reply.status(401).send(err)
       }
     }
-  );
-};
+  )
+}
 
-export default auth;
+export default auth
